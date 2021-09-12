1604fc2ac41aabe97a2b92403b61f5d9bc7560d1
Gabriel Hammer <gabriel.hammer523@gmail.com>
2021-09-12 13:55:12 -0300

    Heroku

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { Server } from "socket.io";
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected,  connection id: ', socket.id)
+
+  socket.on('send message', (message) => {
+    io.emit('receive message', message)
+  })
+
+
+  socket.on('disconnect', () => {
+    console.log('A user Disconnected.');
+  })
+
+});
+
+server.listen(process.env.PORT || 8000, () => {
+  console.log('listening on port :8000');
+});
